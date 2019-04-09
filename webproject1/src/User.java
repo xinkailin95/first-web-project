fc71ed7bf38c1bec0f329803468ab4fadf188f91
xinkai <xinkailin1995@gmail.com>
2019-04-09 00:03:14 -0400

    delete garbage

--- webproject1/src/User.java
+++ webproject1/src/User.java
@@ -1,6 +1,5 @@
 /**
  * This User class only has the username field in this example.
- * <p>
  * However, in the real project, this User class can contain many more things,
  * for example, the user's shopping cart items.
  */
